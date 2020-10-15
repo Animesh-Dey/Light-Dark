5659647c0d0e264d54c102bf9c5ac0f64933e312
ANIMESH DEY <69170945+Animesh-Dey@users.noreply.github.com>
2020-10-16 01:37:45 +0530

    Add files via upload

---
+++ ld.js
@@ -0,0 +1,59 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+// Dark or Light Images
+function imagedark() {
+  image1.src = 'undraw_Marketing_re_7f1g.png';
+  image2.src = 'undraw_Operating_system_re_iqsc.png';
+  image3.src = 'undraw_Designer_mindset_re_2w1k.png';
+}
+function imagelight()
+{
+  image1.src = 'undraw_light.png';
+  image2.src = 'undraw_light (2).png';
+  image3.src = 'undraw_conceptual_idea_xw7k.png';
+}
+// Dark Mode Styles
+function darkMode() {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+  imagedark();
+}
+// Light Mode Styles
+function lightMode() {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+  textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+  toggleIcon.children[0].textContent = 'Light Mode';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+  imagelight();
+}
+// Switch Theme Dynamically
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+    darkMode();
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+    lightMode();
+  }
+}
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
